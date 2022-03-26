887bed1a34ad089dc1e476e1a683f9a23d25413a
owl <scottie20170101@gmail.com>
2022-03-26 20:45:02 +0900

    Stick仮完成

--- PartsView/StickView.swift
+++ PartsView/StickView.swift
@@ -10,60 +10,19 @@ import SwiftUI
 
 
 struct StickView: View {
-    @State private var CurrentPos: CGPoint = CGPoint(x:100, y:100)
-    @State private var CurrentPosEnd: CGPoint = CGPoint(x:200, y:100)
     
-
-
     var body: some View {
         
         
         VStack {
             
-            ZStack{
-                
-                    Path { path in
-                        path.move(to: CGPoint(x: CurrentPos.x, y: CurrentPos.y))
-                        path.addLine(to: CGPoint(x: CurrentPosEnd.x , y: CurrentPosEnd.y))
-                    }
-                    .stroke(lineWidth: 1)
-                    .fill(Color.black)
-                
-                    Path { path in
-                        path.move(to: CGPoint(x: CurrentPos.x, y: CurrentPos.y))
-                        path.addLine(to: CGPoint(x: CurrentPosEnd.x , y: CurrentPosEnd.y))
-                    }
-                    .stroke(lineWidth: 1)
-                    .fill(Color.gray)
-                
-                
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 10, height: 10)
-                    .position(x: CurrentPosEnd.x, y: CurrentPosEnd.y)
-                
-                    .gesture(
-                        DragGesture(minimumDistance: 0, coordinateSpace: .global)
-                            .onChanged { value in
-                                self.CurrentPosEnd = value.location
-                            })
-                Circle()
-                    .fill(Color.blue)
-                    .frame(width: 10, height: 10)
-                    .position(x: CurrentPos.x, y: CurrentPos.y)
-            }
-            .gesture(
-                DragGesture(minimumDistance: 0, coordinateSpace: .global)
-                    .onChanged { value in
-                        self.CurrentPos = value.location
-                    })
+            StickPartsView()
+            
         }
         
-       
+        
     }
-
-    }
-
+}
 
 
 
@@ -71,5 +30,93 @@ struct StickView: View {
 struct StickView_Previews: PreviewProvider {
     static var previews: some View {
         StickView()
+    }
+}
+
+
+
+struct StickPartsView: View {
+    @State private var InitCurrentPos: CGPoint = CGPoint(x:0, y:0)
+    @State private var CurrentPos: CGPoint = CGPoint(x:200, y:100)
+    @State private var CurrentPosBlue: CGPoint = CGPoint(x:300, y:0)
+    @State private var CurrentPosRed: CGPoint = CGPoint(x:100, y:100)
+    @State private var RedSaved: CGPoint = CGPoint(x:100, y:100)
+    @State private var BlueSaved: CGPoint = CGPoint(x:300, y:100)
+    
+    
+    var body: some View {
+        
+        
+        
+        ZStack {
+            Path { path in
+                path.move(to: CGPoint(x: CurrentPosRed.x, y: CurrentPos.y))
+                path.addLine(to: CGPoint(x: CurrentPosBlue.x, y: CurrentPos.y))
+            }
+            .stroke(lineWidth: 10)
+            .gesture(DragGesture(minimumDistance: 10)
+                        .onChanged{ value in
+                
+                self.InitCurrentPos = value.startLocation
+                self.CurrentPos = value.location
+                
+                if InitCurrentPos.x < RedSaved.x {
+                CurrentPosRed.x = CurrentPos.x + (InitCurrentPos.x - RedSaved.x)
+                } else {
+                    CurrentPosRed.x = CurrentPos.x - (InitCurrentPos.x - RedSaved.x)
+                }
+                
+                if InitCurrentPos.x < BlueSaved.x {
+                CurrentPosBlue.x = CurrentPos.x - (InitCurrentPos.x - BlueSaved.x)
+                } else {
+                    CurrentPosBlue.x = CurrentPos.x + (InitCurrentPos.x - BlueSaved.x)
+                }
+                
+                
+                
+   
+            })
+            
+            
+            Circle()
+                .fill(Color.red)
+                .frame(width: 10, height: 10)
+                .position(x: CurrentPosRed.x, y: CurrentPos.y)
+                .gesture(DragGesture(minimumDistance: 0)
+                            .onChanged{ value in
+                    self.CurrentPosRed = value.location
+                }
+                            .onEnded{value in
+                    self.RedSaved = value.predictedEndLocation
+                })
+                
+            
+            Circle()
+                .fill(Color.blue)
+                .frame(width: 10, height: 10)
+                .position(x: CurrentPosBlue.x, y: CurrentPos.y)
+                .gesture(DragGesture(minimumDistance: 0)
+                            .onChanged{ value in
+                    self.CurrentPosBlue = value.location
+                    
+                }
+                            .onEnded{value in
+                    self.BlueSaved = value.predictedEndLocation
+                })
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+}
+
+
+struct ExtractedView: View {
+    var body: some View {
+        StickPartsView()
     }
 }
